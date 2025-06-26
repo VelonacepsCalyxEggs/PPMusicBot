ddb72d7c8e77fb5374db69978b89386a90f3c69b
VelonacepsCalyxEggs <ivankuvaev777@gmail.com>
2025-06-26 19:40:51 +0300

    Update start.bat to correct directory and use npm to start the bot

--- start.bat
+++ start.bat
@@ -1,9 +1,10 @@
 @echo off
 :loop
 :: CHANGE THIS CD TO YOUR DIRECTORY!!!
-cd C:\Server\DSMBot\PP_DMB_TS\dist
+:: This better be nuked sooner or later...
+cd C:\Server\DSMBot\PP_DMB_TS
 echo Starting Discord bot...
-node index.js
+npm run start
 echo Bot crashed with exit code %errorlevel%. Restarting...
 timeout /t 5
 goto loop
