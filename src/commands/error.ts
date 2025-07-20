e03672c9edf77e2a7bf8ecef32be9da3322453a4
VelonacepsCalyxEggs <ivankuvaev777@gmail.com>
2025-07-20 15:53:49 +0300

    Simplify return type of execute method in ErrorCommand to void

--- src/commands/error.ts
+++ src/commands/error.ts
@@ -6,7 +6,7 @@ export default class ErrorCommand extends CommandInterface {
         .setName('error')
         .setDescription('A debug command, that throws a fatal error to test the error handling system.');
 
-    execute = async () : Promise<void | import('discord.js').InteractionResponse | import('discord.js').Message> => {
+    execute = async () : Promise<void> => {
         throw new Error('This is a test error for debugging purposes.');
     }
 };
