e83f758b90f1680d2ff8c29f43d1ee789439b48b
VelonacepsCalyxEggs <ivankuvaev777@gmail.com>
2025-08-02 12:31:25 +0300

    Refactor RemoveCommand to improve descriptions and optimize track removal logic
    although tbh it can probably be optimized thurther by using queue.tracks and it's methods, but I remember having some problems with it in the past, so it's probably best to leave it for later...

--- src/commands/remove.ts
+++ src/commands/remove.ts
@@ -8,38 +8,32 @@ export default class RemoveCommand extends CommandInterface {
     public static readonly commandName = 'remove';
     data = new SlashCommandBuilder()
         .setName(RemoveCommand.commandName)
-        .setDescription('removes the track with an index.')
+        .setDescription('Removes a track on a position or within a range.')
         .addNumberOption(option =>
             option.setName('position')
-                .setDescription('index')
+                .setDescription('Position from where to start removing tracks')
                 .setRequired(true)
         )
         .addNumberOption(option =>
             option.setName('position2')
-                .setDescription('index 2')
+                .setDescription('Position from where to stop removing tracks')
                 .setRequired(false)
         )
     execute = async ({ interaction }: { client: Client; interaction: ChatInputCommandInteraction }) => {
         if (!interaction.guild || !interaction.guildId)return interaction.followUp({ content: 'You need to be in a guild.', flags: ['Ephemeral'] });
         const queue = useQueue(interaction.guild);
         if (!commandPreRunCheckUtil(interaction, queue)) return;
+        // Test with queue.tracks later
         const tracks = queue!.tracks.toArray();
         const position = interaction.options.getNumber('position') as number;
         const position2 = interaction.options.getNumber('position2');
-        
         if (!position2) {
-            tracks.splice(position, 1);
-            queue!.clear();
-            for (const track of tracks) {
-                queue!.addTrack(track);
-            }
+            queue!.removeTrack(tracks[position])
             return interaction.reply({ content: `The track at position ${position} has been removed!`, flags: ['SuppressNotifications'] });
         } else {
-            tracks.splice(position, position2);
-            queue!.clear();
-            for (const track of tracks) {
-                queue!.addTrack(track);
-            }
+            tracks.slice(position, position2).forEach(track => {
+                queue!.removeTrack(track);
+            });
             return interaction.reply({ content: `The tracks from position ${position} to ${position2} have been removed!`, flags: ['SuppressNotifications'] });
         }
     }
