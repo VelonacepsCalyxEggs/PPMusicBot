7bbab9d0330f59762e1b377dc71b84355ff30285
VelonacepsCalyxEggs <ivankuvaev777@gmail.com>
2025-08-05 14:21:40 +0300

    Refactor playTrackHelper to improve metadata handling and ensure defaults are set correctly

--- src/helpers/playHelper.ts
+++ src/helpers/playHelper.ts
@@ -18,13 +18,24 @@ import TrackMetadata from "../types/trackMetadata";
      */
 export default async function playTrackHelper(result: SearchResult | Track, queue: GuildQueue, interaction: ChatInputCommandInteraction, scoredTrack?: ScoredTrack ): Promise<void> {
         let metadata: TrackMetadata | undefined;
-        if (result instanceof SearchResult) {
-            metadata = result.tracks[0].metadata as TrackMetadata
-        }
-        else if (result instanceof Track) {
-            metadata = result.metadata as TrackMetadata;
-        }
-        else {
+        try {
+            if (result instanceof SearchResult) {
+                metadata = result.tracks[0].metadata as TrackMetadata
+            }
+            else if (result instanceof Track) {
+                metadata = result.metadata as TrackMetadata;
+            }
+            else {
+                metadata = {
+                    interaction,
+                    startedPlaying: new Date(),
+                    scoredTrack: undefined,
+                    duration_ms: (result as SearchResult).tracks[0].durationMS || 0 || (result as Track).durationMS,
+                    live: false,
+                    duration: (result as SearchResult).tracks[0].duration || (result as Track).duration || '0:00',
+                };
+            }
+        } catch {
             metadata = {
                 interaction,
                 startedPlaying: new Date(),
