ec878766731f8f50157d767a41515194b867c66b
VelonacepsCalyxEggs <ivankuvaev777@gmail.com>
2025-08-05 13:30:20 +0300

    Refactor LeaveCommand to simplify type imports for interaction response

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { EmbedBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { EmbedBuilder, ChatInputCommandInteraction, InteractionResponse, Message } from 'discord.js';
 import { useQueue } from 'discord-player';
 import CommandInterface from '../types/commandInterface';
 import commandPreRunCheckUtil from '../utils/commandPreRunCheckUtil';
@@ -10,7 +10,7 @@ export default class LeaveCommand extends CommandInterface {
         .setName(LeaveCommand.commandName)
         .setDescription('Drops the queue and leaves from the channel.')
 
-    execute = async ({ interaction }: { interaction: ChatInputCommandInteraction }) : Promise<void | import('discord.js').InteractionResponse | import('discord.js').Message> => {
+    execute = async ({ interaction }: { interaction: ChatInputCommandInteraction }) : Promise<void | InteractionResponse | Message> => {
         // Get the queue for the server
         if (!interaction.guild || !interaction.guildId) {
             return interaction.followUp({content: 'You need to be in a guild.', flags: ['Ephemeral']});
